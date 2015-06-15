d02be9eeb01bc61636dbf085e3d1e6345763b5d7
Rakesh Sharma <raks437@gmail.com>
2015-06-16 01:04:53 +0530

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tap Drag
+==================
+
+An accessible drag-drop solution. Compliant with the WCAG 2.0 standards.
+
+## Install
---
+++ tap-drag.js
@@ -0,0 +1,30 @@
+var tapDrag = (function(window, document, jQuery){
+
+    this.config = {
+        draggable: ".ui-draggable",
+        droppable: ".ui-droppable"
+    };
+
+    this.getDraggable = function(){
+        return this.config.draggable;
+    };
+
+    this.getDroppable = function(){
+        return this.config.droppable;
+    };
+
+    this.setIndex = function () {
+
+    };
+
+    this.setARIA = function () {
+
+    };
+
+    this.begin = function(){
+
+    };
+
+    module.exports = tapDrag;
+
+})(window, document, jQuery);
