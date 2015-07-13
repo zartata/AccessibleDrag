2b2ae8e57471455d9b897ed11bc6a4d6bc3e977b
Rakesh Sharma <raks437@gmail.com>
2015-07-13 20:42:38 +0530

    updated function

--- src/accessibleDrag.js
+++ src/accessibleDrag.js
@@ -5,24 +5,52 @@
     } else {
         window.AccessibleDrag = factory(jQuery, window, document);
     }
-})(function (win, doc, $) {
-    function AccessibleDrag(){
-        var AD = this;
-        var link = $("a[href]");
-        var draggable = function () {
-            return $("*").filter(function () {
-                return $(this).data("ui-draggable");
-            });
-        };
-        var droppable = function () {
-            return $("*").filter(function () {
-                return $(this).data("ui-droppable");
-            });
-        };
+})(function ($, window, document) {
+    //showoff
+    console.log("Accessible Drag\nMade with ❤ in India.");
+    //helper function
+    function getAllDraggable(){
+        return $("*").filter(function () {
+          return $(this).data("ui-draggable");
+        });
+    }
 
-        link.prop("tabIndex", "3");
+    function getAllDroppable(){
+        return $("*").filter(function () {
+            return $(this).data("ui-droppable");
+        });
+    }
+    //main function
+    function AccessibleDrag(elem1, elem2){
+        var drag, drop;
+        if($(elem1).data("ui-draggable") && $(elem2).data("ui-droppable")){
+            drag = $(elem1);
+            drop = $(elem2);
+        }
+        else if($(elem2).data("ui-draggable") && $(elem1).data("ui-droppable")){
+            drag = $(elem2);
+            drop = $(elem1);
+        }
+        else{
+            console.log("Either the drag or drop is not initialized");
+        }
+        //Stage 1
+        drag.prop("tabIndex",1);
+        //Stage 2
+        drag.off().on("keyup",function(){
+            var dragClone;
+            if ( event.which == 13 ) {
+                event.preventDefault();
+                if(drag.is(":focus")){
+                    dragClone = $(this).clone();
+                    console.log(dragClone);
+                }
+            }
+        });
 
     }
+
+
 
     return AccessibleDrag;
 });
